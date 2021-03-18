97506288d29c6c331b04335f91de2b8ba2c5a1ef
jinfutu <jinfutu@i9i8.com>
2021-03-18 17:36:08 +0800

    fix:修改代码

--- src/PushLog.php
+++ src/PushLog.php
@@ -28,7 +28,7 @@ class PushLog
      */
     protected function setConfig()
     {
-        $this->config = $this->app['config']['log_pusher'];
+        $this->config = $this->app['config']['logpusher'];
     }
 
     /**
